54c7145a3f1dae4f3a8d865c5fd4edc0ac3bda5c
joakmannn <165683883+joakmannn@users.noreply.github.com>
2026-01-13 10:43:35 +0100

    create movieMovieList

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
+import Movie from "./components/Movie"
+
 const App = () => {
+  const movieList = []
+
+    for (let index = 0; index <100; index++){
+      movieList.push(<Movie />)
+    }
+
+  
 
   return (
-    <div className="p-10">
-      <button className="btn btn-primary">
-        Hello world
-      </button>
+    <div>
+      <div className="movies flex items-center justify center gap-3 h-screen">
+        {movieList}
+      </div>
     </div>
   )
 }
