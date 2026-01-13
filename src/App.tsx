86943121ddf3c2f3a0e4a6c62dbc680d050a0075
joakmannn <165683883+joakmannn@users.noreply.github.com>
2026-01-13 10:54:10 +0100

    add props

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,20 @@
 import Movie from "./components/Movie"
 
 const App = () => {
-  const movieList = []
+  //const movieList = []
 
-    for (let index = 0; index <100; index++){
-      movieList.push(<Movie />)
-    }
+   // for (let index = 0; index <100; index++){
+   //   movieList.push(<Movie />)
+   // }
 
-  
 
   return (
     <div>
-      <div className="movies flex items-center justify center gap-3 h-screen">
-        {movieList}
+      <div className="movies flex items-center justify-center gap-3 h-screen">
+        <Movie title="Ironman"/>
+        <Movie title="HulK"/>
+        <Movie title="Avengers"/>
+
       </div>
     </div>
   )
